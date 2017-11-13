63f6a35f062377f9bb201e00321364cf932481db
Alex <hinze.alex@gmail.com>
2017-11-13 13:50:31 +0100

    foo

--- catkin_ws/src/ub3/scripts/ub3.py
+++ catkin_ws/src/ub3/scripts/ub3.py
@@ -19,9 +19,32 @@ signal.signal(signal.SIGINT, signal_handler)
 def init():
     global bridge
     rospy.init_node('foo', anonymous=True)
-    #/app/camera/color/image_raw
     rospy.Subscriber("/app/camera/color/image_raw",Image,cameraRawCallback)#subscribe to cars camera
     bridge = CvBridge() #create a opencv <->ros bridge to convert images
+
+
+def calcVR_VT(cv_bin_image):
+    model_points = np.array([(60,0,0), (60,20,0), (30,0,0), (30,20,0), (0,0,0) , (0,20,0)], dtype = "double")
+
+    fx = 614.1699
+    fy = fx
+    cx = 329.9491
+    cy = 237.2788
+
+    camera_matrix = np.matrix(      [[fx,0,cx],
+                                    [0,fy,cy],
+                                    [0,0,1]], dtype = "double")
+    k1 = 0.1115
+    k2 = -0.1089
+    t1 = 0
+    t2 = 0
+
+    distortion_params = np.array([[k1],[k2],[t1],[t2]], dtype = "double")
+
+    image_points = np.array(getPoints(cv_binary_image),  dtype = "double")
+    (retval, rvec, tvec) = cv2.solvePnP(model_points,image_points,camera_matrix,distortion_params)
+
+    return (rvec, tvec)
 
 #callback function to catch images from cars camera
 def cameraRawCallback(data):
@@ -30,38 +53,16 @@ def cameraRawCallback(data):
     global bridge
     cv_input_image = bridge.imgmsg_to_cv2(data, "bgr8") #convert ros image to opencv image
     cv_gray_image = cv2.cvtColor(cv_input_image, cv2.COLOR_BGR2GRAY) #convert to gray scale
-    _,cv_binary_image = cv2.threshold(cv_gray_image,220,255,cv2.THRESH_BINARY)#+cv2.THRESH_OTSU) #binarize image into black and white
+    _,cv_binary_image = cv2.threshold(cv_gray_image,220,255,cv2.THRESH_BINARY) #binarize image into black and white
 
-    image_2d_points = getPoints(cv_binary_image)
-    print image_2d_points
-
-    realworld_3d_points = [()]
-
-    fx = 614.1699
-    fy = fx
-    cx = 329.9491
-    cy = 237.2788
-
-    intrinsic_params = np.asarray([[fx,0,cx],[0,fy,cy],[0,0,1]])
-
-    k1 = 0.1115
-    k2 = -0.1089
-    t1 = 0
-    t2 = 0
-
-    #distortion_params = cv2.CreateMat(3, 3, cv2.CV_32FC3)
-    distortion_params = np.asarray([k1,k2,t1,t2])
-
-    (retval, rvec, tvec) = cv2.solvePnP(realworld_3d_points,image_2d_points,intrinsic_params,distortion_params)
+    (r,t) = calcVR_VT(cv_binary_image)
 
     cv2.imwrite("/home/alex/repos/robotik_ws1718/ub3/cam_image_gray.png",cv_gray_image)
     cv2.imwrite("/home/alex/repos/robotik_ws1718/ub3/cam_image_rgb.png",cv_input_image)
     cv2.imwrite("/home/alex/repos/robotik_ws1718/ub3/cam_image_binary.png",cv_binary_image)
+
     cv_detected_img = cv_input_image.copy()
-    i = 1
-    for center in image_2d_points:
-        cv2.circle(cv_detected_img,center, 6,(0,0,255),3) #draw a red circle on the detected white square
-        cv2.imwrite("/home/alex/repos/robotik_ws1718/ub3/cam_image_detected" + str(i) ".png",cv_detected_img)
+
 
 
 def pubImages(img_gray, img_bin):
@@ -75,11 +76,35 @@ def pubImages(img_gray, img_bin):
     gray_scale_pub.publish(ros_image_gray)
     bin_pub.publish(ros_image_bin)
 
+def get2HighestIndex(l):
+    m1 = max(l)
+    m2 = max(x for x in l if x < m1)
+    i1 = l.index(m1)
+    i2 = l.index(m2)
+    return (i1,i2)
+
+
+def get2LowestIndex(l):
+    m1 = min(l)
+    m2 = min(x for x in l if x > m1)
+    i1 = l.index(m1)
+    i2 = l.index(m2)
+    return (i1,i2)
+
+def get2MidIndex(l):
+    (i1,i2) = get2HighestIndex(l)
+    (i3,i4) = get2LowestIndex(l)
+    r = []
+    for i in range(6):
+        if i != i1 and i != i2 and i != i3 and i != i4:
+            r.append(i)
+    return (r[0],r[1])
+
 
 
 def getPoints(bin_image):
     image, contours, hierarchy = cv2.findContours(bin_image,1, 2)#find contours in image
-    #contours = sorted(contours, key = cv2.contourArea, reverse = True)[0:6] #take the 6 biggest contours
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[0:6] #take the 6 biggest contours
     points = []
     print len(contours)
     xs = []
@@ -90,24 +115,53 @@ def getPoints(bin_image):
         if M['m00'] != 0:#calculate center of contour
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
-            points.append((cx,cy))
             xs.append(cx)
             ys.append(cy)
-    #[(473, 335), (283, 235), (433, 222), (636, 292), (638, 63), (316, 185)]
-    #get first two points/objects, smallest Y-Koordinate
+            points.append((cx,cy))
 
-    upper_left = []
-    upper_right = []
-    middl_left = []
-    middl_right = []
-    lower_left = []
-    lower_right = []
+    (u1,u2) = get2LowestIndex(ys)
+    if xs[u1] < xs[u2]:
+        upper_left = u1
+        upper_right =  u2
+    else:
+        upper_right = u1
+        upper_left = u2
 
-    return points
+    (l1,l2) = get2HighestIndex(ys)
+    if xs[l1] < xs[l2]:
+        lower_left = l1
+        lower_right = l2
+    else:
+        lower_right = l1
+        lower_left = l2
+
+    (m1,m2) = get2MidIndex(ys)
+    print (m1,m2)
+    if xs[m1] < xs[m2]:
+        mid_left = m1
+        mid_right = m2
+    else:
+        mid_left = m2
+        mid_right = m1
+
+    sorted_points = [points[upper_left], points[upper_right], points[mid_left], points[mid_right],
+            points[lower_left], points[lower_right]]
+    print sorted_points
+    return sorted_points
 
 if __name__ == '__main__':
     try:
+
         init()
         rospy.spin()
+        '''
+        img = cv2.imread("/home/alex/repos/robotik_ws1718/ub3/cam_image_rgb.png")
+        cv_gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to gray scale
+        _,cv_binary_image = cv2.threshold(cv_gray_image,220,255,cv2.THRESH_BINARY)#+cv2.THRESH_OTSU) #binarize image into black and white
+        points = getPoints(cv_binary_image)
+        '''
+
+
+
     except rospy.ROSInterruptException:
         pass
