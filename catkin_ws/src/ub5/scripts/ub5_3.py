9247111fd23eb1e206564847d33e6ac732b6c0ba
sveb36 <sveb36@mi.fu-berlin.de>
2017-11-28 10:54:33 +0100

    added comments

--- catkin_ws/src/ub5/scripts/ub5_3.py
+++ catkin_ws/src/ub5/scripts/ub5_3.py
@@ -25,6 +25,7 @@ def signal_handler(signal, frame):
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
+# initialize ros and subscribers and publishers
 def init():
     global steering_pub
     global speed_pub
@@ -35,17 +36,25 @@ def init():
     rospy.Subscriber("/scan", LaserScan, scanCallback, queue_size=1)
     rospy.sleep(1)
 
+# method to get the correct scan/ranges array mapping for a given angle
 def mapAngleToArray(angle,stepping):
     stepping = stepping * 180 / math.pi
     return int(round(angle / stepping))
+
+# method to get the precise angle for a given scan/ranges array mapping
+def mappingToAngle(mapping,stepping):
+    stepping = stepping * 180 / math.pi
+    return mapping * stepping
     
+    
+# a few calculations to get theta* and theta
 def getTheta(dl2, dr2, alpha):
     p = 0.4
     l = 0.5
-    s = 0.2 #nochmal nachmessen
+    s = 0.2
     
     t = math.sqrt(dr2**2 + dl2**2 - 2 * dl2 * dr2 * math.cos(alpha))
-    phi2 = math.asin(t / math.sin(alpha) - 1/dr2)
+    phi2 = math.asin(dr2 * math.sin(alpha) / t)
     do2 = math.sin(phi2) * dl2
     thetal2 = math.asin(do2 / dl2)
     theta = thetal2 - alpha
@@ -54,27 +63,33 @@ def getTheta(dl2, dr2, alpha):
     theta = theta * 180 / math.pi - 90
     return thetaStar, theta
 
-
+# callback of the /scan topic we subscribed to
 def scanCallback(data):
     
     stepping = data.angle_increment
     
+    # get dl2, dr2 for the given angles and get the precise angles they are at
     dl2 = data.ranges[mapAngleToArray(leftAngle, stepping)]
-    leftAngle = mapAngleToArray(leftAngle, stepping) * stepping
+    leftAngle = mappingToAngle(mapAngleToArray(leftAngle, stepping), stepping)
     dr2 = data.ranges[mapAngleToArray(rightAngle, stepping)]
-    rightAngle = mapAngleToArray(rightAngle, stepping) * stepping
+    rightAngle = mappingToAngle(mapAngleToArray(rightAngle, stepping), stepping)
+    # get the angle between dl2 and dr2
     alpha = leftAngle - rightAngle
     
     thetaStar, theta = getTheta(dl2, dr2, alpha)
     deltaHeading = thetaStar - theta
+    # array to calculate derivative and plot heading later
     heading_arr.append(deltaHeading)
+    # array to calculate derivative and plot time later
     t.append(time.time())
     
+    # PD controller calculation from the assignment
     derivative = 0
     if len(heading_arr > 1):
         derivative = (heading_arr[-1] - heading_arr[-2]) / (t[-1] - t[-2])
     u = KP * deltaHeading + KD * derivative + CALIBRATED_ZERO_ANGLE
     
+    # logging
     print "Steering to: ", thetaStar, " from: ", theta
     print "u = ", u
     
