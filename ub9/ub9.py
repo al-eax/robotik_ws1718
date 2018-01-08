d19333afc5acc15ed36c56ea2a5d9049b8b4213c
Alex <hinze.alex@gmail.com>
2018-01-09 00:41:24 +0100

    added some comments

--- ub9/ub9.py
+++ ub9/ub9.py
@@ -20,7 +20,6 @@ from geometry_msgs.msg import PoseArray, Point, Quaternion, Pose
 arena_x = 10.0/2.0
 arena_y = 10.0/2.0
 
-
 balloons = {"purple": (2.255, 2.425),
             "red": (3.515, 3.04),
             "blue": (4.14, 1.79),
@@ -55,7 +54,6 @@ def py_ang(v1, v2):
     sinang = la.norm(np.cross(v1, v2))
     return np.arctan2(sinang, cosang)
 
-
 def create_ros_pose_array_object():
     msg = PoseArray()
     msg.header.stamp = rospy.Time.now()
@@ -67,31 +65,38 @@ def get_pose(x,y,yaw):
     return Pose(Point(x, y, 0), yaw_to_quaternion(yaw))
 
 
+# create random particles
 def initialize_pose_array():
     global pose_array
     pose_array = []
-    for i in range(1000):
+    for i in range(100):
         x = random.uniform(-arena_x, arena_x)
         y = random.uniform(-arena_y, arena_y)
         yaw = random.uniform(-math.pi, math.pi)
         pose_array.append(get_pose(x,y,yaw))
 
 def create_grid():
-    d = {}
+    # 1. create the grid
+    d = {} # this will hold our cells
 
     for i in range(len(pose_array)):
+
         p_x = pose_array[i].position.x
         p_y = pose_array[i].position.y
         p_yaw = quaternion_to_yaw( pose_array[i].orientation)
-        grid_x = round(p_x / 0.25)
-        grid_y = round(p_y / 0.25)
+
+        # calc grid coordinates from particle
+        grid_x = round(p_x * 10 ) #0.25)
+        grid_y = round(p_y * 10 )#0.25)
         coords = (grid_x, grid_y)
 
+        # append particle to corresponding grid
         if coords in d:
             d[coords].append( (p_x, p_y, p_yaw) )
         else:
             d[coords] = [(p_x, p_y, p_yaw)]
 
+    # 2. find the cell with most particles:
     top_cell = []
     top_key = None
     for key in d:
@@ -99,15 +104,19 @@ def create_grid():
         if len(lst) > len(top_cell):
             top_cell = lst
             top_key = key
+    # 3. sum x,y,yaw
     sum_y = 0
     sum_x = 0
     sum_yaw_cos = 0
     sum_yaw_sin = 0
+
     for (x,y,yaw) in top_cell:
         sum_x += x
         sum_y += y
         sum_yaw_sin += math.sin(yaw)
         sum_yaw_cos += math.cos(yaw)
+
+    # 4. calc average
     print "len", len(top_cell)
     avg_x = sum_x / len(top_cell)
     avg_y = sum_y / len(top_cell)
@@ -116,62 +125,40 @@ def create_grid():
     return (avg_x,avg_y,avg_yaw)
 
 
-
-
-
 def calc_weight(ptcl, bln_screen_array):
     (p_x,p_y,p_yaw) = ptcl
 
     final_weight = 1
-    std = 0.1
+    std = 10
 
+    # car position in image
     (car_x, car_y) = (640 / 2, 480 / 2)
 
-    #mit yaw
     for color, (bln_screen_x, bln_screen_y) in bln_screen_array:
-
         (bln_world_x, bln_world_y) = balloons[color]
 
+        # vec car to ballon in image
         rel_car_x = bln_screen_x - car_x
         rel_car_y = bln_screen_y - car_y
 
-        car_angle = angle(rel_car_x,rel_car_y)
+        # vec (0,-1) to calc angle angle in real world
+        y_minus = np.array((0,-1))
+        rel_car = np.array((rel_car_x,rel_car_y))
+        car_angle = py_ang(y_minus, rel_car)
 
-
+        # vec particle to ballon in real world coordinates
         rel_ptcl_x = bln_world_x - p_x
         rel_ptcl_y = bln_world_y - p_y
+
         ptcl_arr = np.array((rel_ptcl_x, rel_ptcl_y))
         ptcl_yaw_vec = np.array((cos(p_yaw),sin(p_yaw)))
 
         ptcl_angle = py_ang(ptcl_arr, ptcl_yaw_vec)
-
-
+        #calc weight
         w = math.exp( -((car_angle - ptcl_angle)**2 / std))
-
-
-
-
-        #car_angle = car_angle - c_yaw
-        #ptcl_angle = ptcl_angle - p_yaw
-
-        #car_arr = np.array((rel_car_x, rel_car_y))
-        #ptcl_arr = np.array((rel_ptcl_x, rel_ptcl_y))
-        #car_yaw_arr = np.array((cos(c_yaw), sin(c_yaw)))
-        #ptcl_yaw_arr = np.array((cos(p_yaw), sin(p_yaw)))
-
-        #car_angle_v2 = py_ang(car_arr, car_yaw_arr)
-        #ptcl_angle_v2 = py_ang(ptcl_arr, ptcl_yaw_arr)
-
-        #w2 = math.exp( -((car_angle_v2 - ptcl_angle_v2)**2 / std))
-
-        #print "car_angle = ", car_angle, "; car_angle_v2 = ", car_angle_v2
-        #print "ptcl_angle = ", ptcl_angle, "; ptcl_angle_v2 = ", ptcl_angle_v2
-        #print "w = ", w, "; w2 = ", w2
-        #print "-------------------------------------------------------"
-
         final_weight = w * final_weight
-
     return final_weight
+
 
 def norm_weights(weights):
     s = np.sum(weights)
@@ -201,7 +188,6 @@ def resample(weights):
         pose_obj = get_pose(pose_array[hold[i]].position.x, pose_array[hold[i]].position.y, quaternion_to_yaw(pose_array[hold[i]].orientation))
         pose_array[i] = pose_obj
 
-
 def create_odom_obj(x,y,yaw):
     msg = Odometry()
     msg.header.stamp = rospy.Time.now()
@@ -211,15 +197,17 @@ def create_odom_obj(x,y,yaw):
 
 
 def odom_callback(data):
+    data.header.stamp = rospy.Time.now()
+    odom_now_pub.publish(data)
+
     global old_x
     global old_y
     global old_yaw
+    #odom data from car
     x = data.pose.pose.position.x
     y = data.pose.pose.position.y
     yaw = quaternion_to_yaw(data.pose.pose.orientation)
 
-    # to move each particle by the motion vector of the car,
-    # every particle must have the same yaw as the car, no?
     try:
         delta_x = x - old_x
         delta_y = y - old_y
@@ -233,15 +221,16 @@ def odom_callback(data):
 
     weights = []
     for i in range(len(pose_array)):
-        # adding some deviation to x and y
-        current_yaw = quaternion_to_yaw(pose_array[i].orientation) + delta_yaw # - wegen spiegelung
+        current_yaw = quaternion_to_yaw(pose_array[i].orientation) - delta_yaw
+        # add some noise to angle:
         current_yaw = norm_rad(random.uniform(0.995 * current_yaw, 1.005 * current_yaw))
 
+        vector_length = math.sqrt(delta_x ** 2 + delta_y ** 2)
+
+        #make yaw to vector to scale it by vector_length
         yaw_vector = np.array((cos(current_yaw), sin(current_yaw)))
 
-        vector_length = math.sqrt(delta_x ** 2 + delta_y ** 2)
-        # add noise to vector
-
+        # add some noise to vector
         vector_length = random.uniform(0.75 * vector_length, 1.25 * vector_length)
 
         yaw_vector = yaw_vector * vector_length
@@ -253,11 +242,12 @@ def odom_callback(data):
     old_x = x
     old_y = y
     old_yaw = yaw
+
     pub_pos.publish(create_ros_pose_array_object())
+
     (gx,gy,gyaw) = create_grid()
     odom = create_odom_obj(gx,gy,gyaw)
     odom_pub.publish(odom)
-
 
 
 def callback(data):
@@ -268,13 +258,10 @@ def callback(data):
     bln_screen_coords = []
     for balloon in bln_array:
         bln_screen_coords.append((balloon[0][0], balloon[1]))
-    #print bln_screen_coords
-    #print len(bln_screen_coords)
-    #print "-------------------------------------------------------------"
 
     weights = []
     for i in range(len(pose_array)):
-        weight = calc_weight((pose_array[i].position.x,pose_array[i].position.y, quaternion_to_yaw(pose_array[i].orientation)), bln_screen_coords)
+        weight = calc_weight((pose_array[i].position.x, pose_array[i].position.y, quaternion_to_yaw(pose_array[i].orientation)), bln_screen_coords)
         weights.append(weight)
     final_weights = norm_weights(weights)
     resample(final_weights)
@@ -286,8 +273,9 @@ def main():
     global bridge
     global detector
     global odom_pub
-
-    odom_pub = rospy.Publisher("/assignment6/odom", Odometry, queue_size=200)
+    global odom_now_pub
+    odom_pub = rospy.Publisher("/mcpf_gps", Odometry, queue_size=200)
+    odom_now_pub = rospy.Publisher("/odom_now", Odometry, queue_size=200)
     rospy.init_node('my_little_nodey', anonymous=True)
 
     pub_pos = rospy.Publisher('/mcposearray', PoseArray, queue_size=1)
@@ -296,18 +284,8 @@ def main():
     bridge = CvBridge()
 
     rospy.Subscriber("/odom", Odometry, odom_callback, queue_size=1)
-
     detector = BalloonDetector()
-
     rospy.Subscriber("/usb_cam/image_rect_color/compressed", CompressedImage, callback, queue_size=1)
-
-
-    #rate = rospy.Rate(10.0)
-    #while not rospy.is_shutdown():
-        #ros_pose_obj = create_ros_pose_array_object(pose_array)
-        #pub_pos.publish(ros_pose_obj)
-        #rate.sleep()
-
     rospy.spin()
 
 
